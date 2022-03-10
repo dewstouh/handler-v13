37049f7918f67b9abe163d7499c0f11bd31df5b6
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-03-10 21:41:44 +0100

    Update messageCreate.js

--- eventos/guild/messageCreate.js
+++ eventos/guild/messageCreate.js
@@ -1,6 +1,6 @@
 const config = require(`${process.cwd()}/config/config.json`)
 const serverSchema =  require(`${process.cwd()}/modelos/servidor.js`)
-const {asegurar, asegurar_todo} = require(`${process.cwd()}/handlers/funciones.js`)
+const {asegurar_todo} = require(`${process.cwd()}/handlers/funciones.js`)
 module.exports = async (client, message) => {
     if(!message.guild || !message.channel || message.author.bot) return;
     await asegurar_todo(message.guild.id, message.author.id);
