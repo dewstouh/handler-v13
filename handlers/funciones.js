6e31e569b37821c9652c4b5857592c65ab88194a
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-03-10 21:43:00 +0100

    Update funciones.js
    Eliminada función asegurar y reemplazada por asegurar_todo

--- handlers/funciones.js
+++ handlers/funciones.js
@@ -1,13 +1,19 @@
+const serverSchema = require(`${process.cwd()}/modelos/servidor.js`);
+const config = require(`${process.cwd()}/config/config.json`);
 module.exports = {
-    asegurar,
+    asegurar_todo,
 }
 
-async function asegurar(schema, id, id2, objeto){
-    let data = await schema.findOne({id: id2})
-    if(!data){
-        console.log("NO HABIA BASE DE DATOS CREADA, CREANDO UNA...")
-        data = await new schema(objeto)
-        data.save();
+async function asegurar_todo(guildid, userid){
+    if (guildid) {
+        let serverdata = await serverSchema.findOne({ guildID: guildid })
+        if (!serverdata) {
+            console.log(`Asegurado: Config de Server`.green);
+            serverdata = await new serverSchema({
+                guildID: guildid,
+                prefijo: config.prefix
+            });
+            await serverdata.save();
+        }
     }
-    return data;
 }
