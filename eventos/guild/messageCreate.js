7f3af85771dbd56402aaa471670bc9c4f89aa533
dewstouh <53043542+dewstouh@users.noreply.github.com>
2022-03-10 21:41:37 +0100

    Optimizado + Arreglado bugs

--- eventos/guild/messageCreate.js
+++ eventos/guild/messageCreate.js
@@ -1,12 +1,10 @@
 const config = require(`${process.cwd()}/config/config.json`)
 const serverSchema =  require(`${process.cwd()}/modelos/servidor.js`)
-const {asegurar} = require(`${process.cwd()}/handlers/funciones.js`)
+const {asegurar, asegurar_todo} = require(`${process.cwd()}/handlers/funciones.js`)
 module.exports = async (client, message) => {
     if(!message.guild || !message.channel || message.author.bot) return;
-    let data = await asegurar(serverSchema, "guildID", message.guild.id, {
-        guildID: message.guild.id,
-        prefijo: config.prefix
-    });
+    await asegurar_todo(message.guild.id, message.author.id);
+    let data = await serverSchema.findOne({guildID: message.guild.id})
     if(!message.content.startsWith(data.prefijo)) return;
     const args = message.content.slice(data.prefijo.length).trim().split(" ");
     const cmd = args.shift()?.toLowerCase();
